2623798dcb326e7ea12b0c6ad6c1970c85cb7a56
patsukaa <aezhik@mail.ru>
2021-11-05 19:42:06 +0300

    init commit

---
+++ src/test/java/com/github/patsukaa/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.patsukaa.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
